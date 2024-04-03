48b5da9e09411e241c141ff29fc12c67fd9bba15
thewisepeter <rempeterwisdom@gmail.com>
2024-04-03 14:01:14 +0300

    solution to 4-rest-parameter.js

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,6 +1,6 @@
 export default function returnHowManyArguments(...args) {
   let total = 0;
-  for (const arg of args) {
+  for (const arg of args) { // eslint-disable-line no-unused-vars
     total += 1;
   }
   return total;
