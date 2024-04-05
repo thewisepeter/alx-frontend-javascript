798796d156dcb6fa43dcb4cc13a7ee638c03c1d5
thewisepeter <rempeterwisdom@gmail.com>
2024-04-06 02:57:49 +0300

    solution to 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    uploadPhoto(fileName), // Promise for uploading photo
-    signUpUser(firstName, lastName), // Promise for signing up user
-  ])
-    .then((results) => {
-      // Process the result array from Promise.allSettled using .then() handler
-      results.map((result) => ({
-        status: result.status, // Extract status (either 'fulfilled' or 'rejected')
-        value: result.status === 'fulfilled' ? result.value : String(result.reason), // Determine value based on status
-      }));
-    });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
