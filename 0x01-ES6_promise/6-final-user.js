852e0f6650d07d784044177a8ae11efd3e3ca12b
thewisepeter <rempeterwisdom@gmail.com>
2024-04-06 02:46:24 +0300

    solution to 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,11 +6,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
     uploadPhoto(fileName), // Promise for uploading photo
     signUpUser(firstName, lastName), // Promise for signing up user
   ])
-    .then((results) => {
+    .then((results) =>
       // Process the result array from Promise.allSettled using .then() handler
       results.map((result) => ({
         status: result.status, // Extract status (either 'fulfilled' or 'rejected')
         value: result.status === 'fulfilled' ? result.value : String(result.reason), // Determine value based on status
-      }));
-    });
+      })));
 }
